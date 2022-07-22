ce980fed080cf97e6c4b1b88e1bfcc1402057830
Magnus Did Nothing Wrong <knight19jonathan@gmail.com>
2022-07-21 20:40:18 -0700

    can initalize in node.js bugs to html

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -4,16 +4,16 @@ const generateHTML = require('./util/generateHTML');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
+const Employee = require('./lib/Employee');
 const team = [];
 
-const startProgram = () => {
+async function startProgram() {
     const managerForm = await managerQuestions()
-    const manager = new Manager(managerForm.managerName, managerForm.managerId, managerForm.managerEmail, managerForm.managerOfficeNumber);
-    team.push(manager);
-    startTeam();
+    const manager = new Manager(managerForm.managerName, managerForm.managerId, managerForm.managerEmail, managerForm.managerOfficeNumber)
+    team.push(manager)
+    startTeam()
 
 }
-
 async function managerQuestions() {
     const managerFillin = await inquirer.prompt([
         {
@@ -39,13 +39,105 @@ async function managerQuestions() {
     ])
     return managerFillin;
 }
+async function startTeam() {
+    const teamFillin = await teamClassPick();
+    console.log(teamFillin)
+    if(teamFillin === 'Intern') {
+        const internFillin = await internQuestions();
+        const intern = new Intern(internFillin.internName, internFillin.internId, internFillin.internEmail, internFillin.internSchool);
+        team.push(intern);
+    } else if (teamFillin === 'Engineer') {
+        const engineerFillin = await engineerQuestions();
+        const engineer = new Engineer(engineerFillin.engineerName, engineerFillin.engineerId, engineerFillin.engineerEmail, engineerFillin.engineerGithub);
+        team.push(engineer);
+    } else {
+        console.log("Exit!");
+        return;
+    }
+    outputHtml()
+}
+async function teamClassPick() {
+    const teamClass = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'teamOperatorClass',
+            message: 'What type of team member would you like to add?',
+            choices: ['Intern', 'Engineer', 'Quit']
+        }
+    ])
+    return teamClass.teamOperatorClass;
+}
+async function internQuestions() {
+    const internFillin = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: 'What is the name of the intern?'
+        },
+        {
+            type: 'number',
+            name: 'internId',
+            message: 'What is the ID number of this intern?'
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: 'What is the email of this intern?'
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: 'What school is this intern attending?'
+        }
+    ])
+    return internFillin;
+}
+async function engineerQuestions() {
+    const engineerFillin = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: 'What is the name of the engineer?'
+        },
+        {
+            type: 'number',
+            name: 'engineerId',
+            message: 'What is the ID number of this engineer?'
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: 'What is the email of this engineer?'
+        },
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: 'What is the github username of this engineer?'
+        }
+    ])
+    return engineerFillin;
+}
+async function outputHtml() {
+    const newHtml = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'htmlOutput',
+            message: 'Complete filling out team members or create HTML team portfolio?',
+            choices: ['Complete Team', 'Create More Team Members']
+        }])
+    if(newHtml.htmlOutput === 'Create More Team Members') {
+        teamClassPick();
+    } else {
+        const html = generateHTML(team);
 
+        fs.writeFile('./output/team.html', html, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+    }
+}
 
-
-
-
-
-
+startProgram();
 
 
 
